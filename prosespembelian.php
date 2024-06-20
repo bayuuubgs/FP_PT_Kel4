ff23b68b56626ccbf542fb425776ca4dcd90d2a9
NovallFirmansyah <adriannoval2004@gmail.com>
2024-06-20 18:11:52 +0700

    update status pesanan done

--- prosespembelian.php
+++ prosespembelian.php
@@ -8,10 +8,11 @@ $harga= $_POST['harga'];
 $total= $_POST['jumlah'];
 $tipe= $_POST['Gender'];
 $date= $_POST['Date'];
-$totalp= $_POST['totalPembayaran'];
+$totalp = $_POST['totalPembayaran'];
+$status = "Sedang Diproses";
 // Buat query untuk memasukkan data ke database
-$query = "INSERT INTO tb_pesanan (name, email, phone_number, nama_wisata, harga, jumlah, pembayaran, tanggal_kunjung, total) 
-VALUES ('$nama', '$email', '$telp', '$wisata', '$harga', '$total', '$tipe', '$date', '$totalp')";
+$query = "INSERT INTO tb_pesanan (name, email, phone_number, nama_wisata, harga, jumlah, pembayaran, tanggal_kunjung, total, status) 
+VALUES ('$nama', '$email', '$telp', '$wisata', '$harga', '$total', '$tipe', '$date', '$totalp', '$status')";
 
 // Jalankan query dan cek apakah berhasil
 $hasil = mysqli_query($conn, $query);
